3795992b5c5a1c259910486a9a70b2ec20f7a7f5
Fathurrahman Kesuma Ridwan <fathur260605@gmail.com>
2025-03-21 14:05:35 +0700

    (4) Simulation of slow request.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    fs, io::{prelude::*, BufReader},
+     net::{TcpListener, TcpStream},
+      thread,
+      time::Duration,
    };
    
-   fn main() {
+   fn main() {  
         let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
         for stream in listener.incoming() {
@@ -16,11 +17,14 @@ use std::{
         let buf_reader = BufReader::new(&mut stream);
         let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-        let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        let (status_line, filename) = match &request_line[..] {
+            "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+            "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(10));
             ("HTTP/1.1 200 OK", "hello.html")
-        } else {
-            ("HTTP/1.1 404 NOT FOUND", "404.html")
-        };
+        }
+            _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+            };
 
         let contents = fs::read_to_string(filename).unwrap();
         let length = contents.len();
